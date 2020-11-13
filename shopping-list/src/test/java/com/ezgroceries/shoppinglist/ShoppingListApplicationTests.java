9cb3900813e278986267c65375c5e518d5ab06fa
Marc Van Vlaslaer <marc.vanvlaslaer@kbc.be>
2020-11-13 09:53:49 +0100

    lab 2.3

--- shopping-list/src/test/java/com/ezgroceries/shoppinglist/ShoppingListApplicationTests.java
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/ShoppingListApplicationTests.java
@@ -1,15 +1,8 @@
 package com.ezgroceries.shoppinglist;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
 class ShoppingListApplicationTests {
-
-	@Autowired
-	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
